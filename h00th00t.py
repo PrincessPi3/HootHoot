253b6273e18a236657583ac0609b89c010979853
PrincessPi3 <princesspi@proton.me>
2025-08-07 12:09:15 -0600

    doot TO NOT BREAK TERMINAL

--- h00th00t.py
+++ h00th00t.py
@@ -10,7 +10,7 @@ def beacon_raw(SSID, length=255):
 	beacon = Dot11Beacon()
 	essid = Dot11Elt(ID='SSID',info=RawVal(SSID), len=length)
 	frame = RadioTap()/dot11/beacon/essid
-	print(f"{SSID}")
+	print("FIRIN MY LAZORRRRRR")
 	sendp(frame, iface=iface, inter=0.250, loop=1)
 
 ssid_binary = 0b0101 # a few random bits to send as the SSID
